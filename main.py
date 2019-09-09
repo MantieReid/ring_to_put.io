8fb63b5a4f5fe4b4d087e097af6bda3ef4e5433c
monte <mantiereid2@gmail.com>
2019-09-09 18:47:25 -0400

    first commit. made code to get the download links for the last 20 videos.

---
+++ main.py
@@ -0,0 +1,45 @@
+import putiopy
+from ring_doorbell import Ring
+
+
+import config
+
+eventidlist = []
+myring = Ring(config.username,config.password)
+
+#get a list of all devices.
+
+print("list all devices", myring.devices)
+print("list all stickup cams",myring.stickup_cams)
+print("list all chimes", myring.chimes)
+print("list all doorbells", myring.doorbells)
+for doorbell in myring.doorbells:
+
+      # listing the last 20 events of any kind
+      for event in doorbell.history(limit=20):
+        print('ID:       %s' % event['id'])
+
+
+        print('Kind:     %s' % event['kind'])
+        print('Answered: %s' % event['answered'])
+        print('When:     %s' % event['created_at'])
+        print('--' * 50)
+
+        #adds the event IDs to the list.
+        eventidlist.append(event['id'])
+        # print(eventidlist)
+
+print(eventidlist)
+ringdownloadlist = [] # list that will hold the urls of all the download links it
+
+# need to make a counter. That counts the number of items that has been added to the list compared with the eventid list.
+# example 3/5 links added.
+#get the total of the eventid list.  make that the number after the slash.  then make the number in fro
+
+doorbell = myring.doorbells[0]
+print("time to run for loop")
+for x in eventidlist:
+    ringdownloadlist.append(doorbell.recording_url(x))
+    print("list is now" + str(ringdownloadlist))
+
+print(ringdownloadlist)
