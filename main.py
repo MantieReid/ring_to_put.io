07450c12bc54f7fa2b39b27775c7d1dce1731339
monte <mantiereid2@gmail.com>
2019-09-10 19:44:24 -0400

    Managed to get the files to download to put.io. However, I am having trobule getting the script to run.

--- main.py
+++ main.py
@@ -1,45 +1,60 @@
 import putiopy
 from ring_doorbell import Ring
-
-
 import config
 
-eventidlist = []
-myring = Ring(config.username,config.password)
+def main():
 
-#get a list of all devices.
+    eventidlist = []
+    myring = Ring(config.username,config.password)
 
-print("list all devices", myring.devices)
-print("list all stickup cams",myring.stickup_cams)
-print("list all chimes", myring.chimes)
-print("list all doorbells", myring.doorbells)
-for doorbell in myring.doorbells:
+    #get a list of all devices.
 
-      # listing the last 20 events of any kind
-      for event in doorbell.history(limit=20):
-        print('ID:       %s' % event['id'])
+    print("list all devices", myring.devices)
+    print("list all stickup cams",myring.stickup_cams)
+    print("list all chimes", myring.chimes)
+    print("list all doorbells", myring.doorbells)
+    for doorbell in myring.doorbells:
+
+          # listing the last 20 events of any kind
+          for event in doorbell.history(limit=5):
+            print('ID:       %s' % event['id'])
 
 
-        print('Kind:     %s' % event['kind'])
-        print('Answered: %s' % event['answered'])
-        print('When:     %s' % event['created_at'])
-        print('--' * 50)
+            print('Kind:     %s' % event['kind'])
+            print('Answered: %s' % event['answered'])
+            print('When:     %s' % event['created_at'])
+            print('--' * 50)
 
-        #adds the event IDs to the list.
-        eventidlist.append(event['id'])
-        # print(eventidlist)
+            #adds the event IDs to the list.
+            eventidlist.append(event['id'])
+            # print(eventidlist)
 
-print(eventidlist)
-ringdownloadlist = [] # list that will hold the urls of all the download links it
+    print(eventidlist)
+    ringdownloadlist = [] # list that will hold the urls of all the download links it
 
-# need to make a counter. That counts the number of items that has been added to the list compared with the eventid list.
-# example 3/5 links added.
-#get the total of the eventid list.  make that the number after the slash.  then make the number in fro
+    # need to make a counter. That counts the number of items that has been added to the list compared with the eventid list.
+    # example 3/5 links added.
+    #get the total of the eventid list.  make that the number after the slash.  then make the number in fro
 
-doorbell = myring.doorbells[0]
-print("time to run for loop")
-for x in eventidlist:
-    ringdownloadlist.append(doorbell.recording_url(x))
-    print("list is now" + str(ringdownloadlist))
+    doorbell = myring.doorbells[0]
+    print("time to run for loop")
+    for x in eventidlist:
+        ringdownloadlist.append(doorbell.recording_url(x))
+        print("list is now" + str(ringdownloadlist))
 
-print(ringdownloadlist)
+    print(ringdownloadlist)
+
+
+    helper = putiopy.AuthHelper(config.client, config.application_secret,
+                                "https://webhook.site/6e8bc62f-477b-44e6-a67f-9c39795eefb9", type='token')
+
+    client = putiopy.Client(config.token)
+    helper.open_authentication_url()
+
+    for x in eventidlist:
+        transfer = client.Transfer.add_url(str(ringdownloadlist[x]))
+
+
+
+if __name__ == '__main__':
+  main()
