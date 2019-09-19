ca74ba7271af69d7c054e26893baa53d6dc09bdf
mantie <mantiereid2@gmail.com>
2019-09-18 23:56:39 -0400

    Added code to make the while loop better. But having issues with it.

--- testfile.py
+++ testfile.py
@@ -1,13 +1,38 @@
 import putiopy
 from ring_doorbell import Ring
 import config
+from collections import Counter
+
+
+def total_vide_count():
+  print("getting the total number of videos in the ring account")
+
+  user_total_videos = 0
+  # gets the total amount of videos in the ring account.
+  myring = Ring(config.username, config.password)
+  doorbell = myring.doorbells[0]
+
+  events = []
+  counter = 0
+  history = doorbell.history(limit=100)
+  while (len(history) > 0):
+    events += history
+    counter += len(history) # tells us the total amount of videos in the account.
+    history = doorbell.history(older_than=history[-1]['id'])
+  print("total amount of videos is " + str(counter))
+  return counter
+
+
 
 def main():
+    total_vide_count()
+    a = total_vide_count()
     downloasdurl = []
 
     eventidlist = []
     myring = Ring(config.username, config.password)
     doorbell = myring.doorbells[0]
+    counter1 = 0
     for doorbell in myring.doorbells:
 
         # listing the last 15 events of any kind
@@ -19,7 +44,7 @@ def main():
         print("eventidlist is " + str(eventidlist))
         histroy = doorbell.history(limit=100, older_than=eventidlist[-1])
 
-        while(len(eventidlist) < 1000):
+        while(len(eventidlist) < a):
             for event in histroy:
                 # print('ID:       %s' % event['id'])
                 eventidlist.append(event['id'])
